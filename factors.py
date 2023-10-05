f46f8eedfa228ba78bd1d77cda1c4a9647eda023
autumn curry <autumnc34@nycstudents.net>
2023-10-05 13:46:28 -0400

    gcf
    IM DONEE WOOHOOOOO

--- factors.py
+++ factors.py
@@ -4,9 +4,10 @@ factors = []
 def gcf(x,y):
     for i in range(2, y):
         if x%i==0 and y%i==0:
-            factors.append(i) 
-            print(max(factors)) 
+            factors.append(i)
+    print(max(factors))
+    #save button = ctrl s 
 #num and num2 should be x and y
 gcf(num,num2)
-#if you put 0 in "for i in range()", you are starting with 0. if you are diving by 0, you can't start with 0. 
+#if you put 0 in "for i in range()", you are starting with 0. if you are diving by 0, you can't start with 0. ss
 #google append list python. When you append something, you are adding a new item to your existing list. 
